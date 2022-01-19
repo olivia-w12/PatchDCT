d58bb73fa56d4743cb7637f49650998466dda73a
olivia-w12 <503883283@qq.com>
2022-01-19 17:12:05 +0800

    experiment1

---
+++ projects/DCT_Mask_test/dct_mask/config.py
@@ -0,0 +1,13 @@
+
+def add_dctmask_config(cfg):
+    """
+    Add config for DCT-Mask.
+    """
+
+    # For MaskRCNNDCTHead
+    cfg.MODEL.ROI_MASK_HEAD.IN_FEATURES = ["p2"]
+    cfg.MODEL.ROI_MASK_HEAD.DCT_VECTOR_DIM = 300
+    cfg.MODEL.ROI_MASK_HEAD.DCT_VECTOR_DIM_CUT = 10
+    cfg.MODEL.ROI_MASK_HEAD.MASK_SIZE = 128
+    cfg.MODEL.ROI_MASK_HEAD.DCT_LOSS_TYPE = "l1"
+    cfg.MODEL.ROI_MASK_HEAD.MASK_LOSS_PARA = 1.0
