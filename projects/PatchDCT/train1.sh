9ea89331ad14beeeacb5bfbe20ba1549d3bc822b
olivia-w12 <503883283@qq.com>
2022-08-30 10:37:40 +0800

    multi-stage

--- projects/PatchDCT/train1.sh
+++ projects/PatchDCT/train1.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1,2,3,4 \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 python train_net.py --config-file configs/DCT/RX101_32x8d_3x_dct_300_l1_0_007_4conv.yaml \
-                    --num-gpus 4 \
+                    --num-gpus 8 \
                     --dist-url "tcp://127.0.0.1:6020"
