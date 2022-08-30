84159a04e0820047ef8bc2d8ee0c065cf01a55ab
olivia-w12 <503883283@qq.com>
2022-08-30 10:22:18 +0800

    refine DCT vectors

--- projects/PatchDCT/dct_mask/__init__.py
+++ projects/PatchDCT/dct_mask/__init__.py
@@ -1,5 +1,5 @@
 from .config import add_dctmask_config
-# from .mask_encoding import DctMaskEncoding
+from .mask_encoding import DctMaskEncoding
 from .mask_encoding_V2 import DctMaskEncodingV2
 from .patchdct_roi_heads import PatchDCTROIHeads
 from .dct import dct_2d,idct_2d
