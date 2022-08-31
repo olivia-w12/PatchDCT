4e28b3da4a7d05e8e4192f23ceacd08d6b3f0a1b
olivia-w12 <503883283@qq.com>
2022-09-01 00:21:22 +0800

    deconv and biou

--- projects/PatchDCT/eval_biou.sh
+++ projects/PatchDCT/eval_biou.sh
@@ -1,4 +1,4 @@
 python ./biou/tools/coco_instance_evaluation.py \
       --gt-json-file ./datasets/coco/annotations/instances_val2017.json \
-      --dt-json-file ./output/patch_size_4/inference/coco_instances_results.json \
+      --dt-json-file ./output/refine_DCT_vectors/inference/coco_instances_results.json \
       --iou-type boundary
